543f2b2b1a05ad9ab525b27796fa08d76eb9e10a
rubybdse0507 <ruby.lin.sokawai@gmail.com>
2017-09-02 17:00:13 +0800

    Facebook-好市多

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -25,8 +25,8 @@ public class FacebookExam {
 
 		String uri = 
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/search?q==%e5%a5%bd%e5%b8%82%e5%a4%9a%0d%0a&type=page&limit=1000&fields=name,id,likes,talking_about_count"
+				+ "&access_token=EAACEdEose0cBAFKRjcPz9BbZAKsNDgneUSZC6lsBYuZCpdf6Fs4yfoPK3dkmbUufJ6fk8VNQhQd2QMrEXwtaQsuKCf67s2gYgNlSHZCE9CY75DVLZBF4SZBIVtv88LDPvu1YGfGxEIPlrAkUpo2F8HsHKRH9pkHZCoVzj2c84dOUtibyqcO5kI5egZCizC7ZBETcZD";
 
 
 		Elements elems =
@@ -34,17 +34,22 @@ public class FacebookExam {
 				.getFromJson(uri)
 				.select("data");
 		
-		String output = "id,reactions";
+		String output = "id,名稱,按讚數,討論人數\n";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
+			String name = data.select("name").text();
+			String likes = data.select("likes").text();
+			String talking_about_count = data.select("talking_about_count").text();
 
+
+			output += id+",\""+name+"\","+likes+","+talking_about_count+"\n";
 			// FIXIT
-			String reactions = "";
+			//String reactions = "";
 
 
-			output += id + "," + reactions + "\n";
+			//output += id + "," + reactions + "\n";
 		}
 
 		System.out.println( output );
